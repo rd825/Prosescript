b6d0358310f90d306c2c928be953f2e050e8d0e5
Rahul Desai <rd825@georgetown.edu>
2018-12-16 16:22:14 -0500

    Got rid of some comments, updated favicon

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -3,8 +3,6 @@ import {Link, NavLink} from 'react-router-dom';
 
 let Nav = props => {
     return (
-        // The logo links home. The first navlink links home as well. The second navlink goes to the form.
-        // It'd be great to figure out how to switch Add to Edit in the relevant cases.
         <nav>
             <Link to='/'><h1 id='logo'>Prosescript</h1></Link>
             <div className='links'>
