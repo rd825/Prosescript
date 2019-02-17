379f2de0c6f53503ec0c3a341c48ef1fc47caf35
Rahul Desai <rd825@georgetown.edu>
2019-02-17 10:58:15 -0500

    Implementing CloudMailIn processing

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // importing relevant packages
 const express = require("express");
 const axios = require("axios");
-const notifier = require("mail-notifier");
+//const notifier = require("mail-notifier");
 const Cryptr = require("cryptr");
+const formidable = require("formidable");
 const users = require("./db/userModel");
-const imap = require("./config/imapConfig");
+//const imap = require("./config/imapConfig");
 const { create, refresh } = require("./helpers");
 
 // setting up server + middleware
@@ -91,38 +92,64 @@ server.post("/api/auth", (req, res) => {
 // Listen for new emails, read them and process accordingly
 
 server.post("/api/emails", (req, res) => {
-  res.status(201).json(req.body);
+  let form = new formidable.IncomingForm();
+  form.parse(req, function(err, fields, files) {
+    const mailObj = {
+      title: fields.headers["Subject"],
+      html: fields.html,
+      text: fields.plain,
+      email: fields.from
+    };
+    console.log(mailObj);
+    users
+      .getByEmail(fields.from)
+      .then(res => {
+        const { user_id, access_token, refresh_token, expires_at } = res;
+        const now = Date.now();
+
+        const decrypted_access = cryptr.decrypt(access_token);
+        const decrypted_refresh = cryptr.decrypt(refresh_token);
+
+        if (now >= expires_at) {
+          refresh(decrypted_refresh, client_id, client_secret, mailObj);
+        } else {
+          create(user_id, decrypted_access, mailObj);
+        }
+      })
+      .catch(err => console.log(err));
+  });
 });
 
 // const n = notifier(imap);
-// n.on('end', () => n.start()) // session closed
-//     .on('mail', mail => {
-//         const {subject, html, text} = mail;
-//         const email = mail['from'][0]['address'];
-//         const mailObj = {
-//             title: subject,
-//             html: html,
-//             text: text,
-//             email: email,
-//         }
+// n.on("end", () => n.start()) // session closed
+//   .on("mail", mail => {
+//     const { subject, html, text } = mail;
+//     const email = mail["from"][0]["address"];
+//     const mailObj = {
+//       title: subject,
+//       html: html,
+//       text: text,
+//       email: email
+//     };
 
-//         users.getByEmail(email)
-//         .then(res => {
-//             const {user_id, access_token, refresh_token, expires_at} = res;
-//             const now = Date.now();
+// users
+//   .getByEmail(email)
+//   .then(res => {
+//     const { user_id, access_token, refresh_token, expires_at } = res;
+//     const now = Date.now();
 
-//             const decrypted_access = cryptr.decrypt(access_token);
-//             const decrypted_refresh = cryptr.decrypt(refresh_token);
+//     const decrypted_access = cryptr.decrypt(access_token);
+//     const decrypted_refresh = cryptr.decrypt(refresh_token);
 
-//             if (now >= expires_at) {
-//                 refresh(decrypted_refresh, client_id, client_secret, mailObj)
-//             } else {
-//                 create(user_id, decrypted_access, mailObj)
-//             }
-//         })
-//         .catch(err => console.log(err));
-// })
-// .start();
+//     if (now >= expires_at) {
+//       refresh(decrypted_refresh, client_id, client_secret, mailObj);
+//     } else {
+//       create(user_id, decrypted_access, mailObj);
+//     }
+//   })
+//   .catch(err => console.log(err));
+//   })
+//   .start();
 
 const port = process.env.PORT || 9000;
 server.listen(port, () => console.log(`Listening on http://localhost:${port}`));
