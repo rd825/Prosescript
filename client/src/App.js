5ecb9ae3e57680a7aea873f8e8d5c9378f596606
Rahul Desai <rd825@georgetown.edu>
2019-03-03 12:59:16 -0500

    trying to get GA running without react-ga

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
-import ReactGA from "react-ga";
 
 import Nav from "./components/Nav";
 import Home from "./components/Home";
@@ -11,14 +10,6 @@ const clientId = "910cc0297d37";
 const state = "opensesame";
 const redirectUri = "https://prosescript.netlify.com/callback";
 const url = `https://medium.com/m/oauth/authorize?client_id=${clientId}&scope=basicProfile,publishPost&state=${state}&response_type=code&redirect_uri=${redirectUri}`;
-
-ReactGA.initialize("UA-135248853-1", {
-  debug: false, // sends feedback to the console
-  titleCase: false,
-  userId: 135248853,
-  siteSpeedSampleRate: 100 // rate at which data is sent, default is 1
-});
-ReactGA.pageview(`${window.location.pathname}`);
 
 class App extends Component {
   render() {
--- client/src/index.js
+++ client/src/index.js
@@ -6,8 +6,27 @@ import "./reset.css";
 import "./index.css";
 import App from "./App";
 
+const injectGA = () => {
+  if (typeof window == "undefined") {
+    return;
+  }
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    window.dataLayer.push(arguments);
+  }
+  gtag("js", new Date());
+
+  gtag("config", "UA-135248853-1");
+};
+
 ReactDOM.render(
   <Router>
+    {/* Global site tag (gtag.js) - Google Analytics */}
+    <script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=UA-135248853-1"
+    />
+    <script>{injectGA()}</script>
     <App />
   </Router>,
   document.getElementById("root")
